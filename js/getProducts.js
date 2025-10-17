bd7681513e8725de2e5a743fe45d63597b85bde5
Indrawan <indrawanismail0@gmail.com>
2025-10-17 20:33:17 +0800

    feat: fetch data from json

--- js/getProducts.js
+++ js/getProducts.js
@@ -1,9 +1,29 @@
 
 async function getProducts() {
-    const res = await fetch('../data/products.json')
-    const products = await res.json()
-    console.log(products.map(product => product.id))
-    console.log(products)
+    try {
+        const res = await fetch('./data/products.json')
+        const products = await res.json()
+        renderCard(products)
+    } catch(error) {
+        console.log(error)
+    }
 }
 
 getProducts()
+    
+function renderCard(cardData) {
+    const container = document.querySelector('.card-product-container')
+    let cardHTML = ""
+    console.log(cardData)
+    cardData.forEach((card, id) => {
+        cardHTML += `<div class="card-product">
+                    <img src="${card.image}" alt="roti gembung">
+                    <div>
+                        <h4>${card.name}</h4>
+                        <p>${card.description}</p>
+                    </div>
+                </div>`
+                console.log(card.image)
+    });
+    container.innerHTML = cardHTML
+}
